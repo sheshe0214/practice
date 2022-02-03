744d78afdeecaed8c4417bdd56625622ad0702aa
sheshe0214 <95837391+sheshe0214@users.noreply.github.com>
2022-02-03 11:23:14 -0500

    Add files via upload

---
+++ python.add.divide.%.py
@@ -0,0 +1,30 @@
+Python 3.7.1 (v3.7.1:260ec2c36a, Oct 20 2018, 03:13:28) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 2 + 3
+5
+>>> 100 - 20
+80
+>>> 100 + 20 - 80
+40
+>>> 2 * 2
+4
+>>> 2 / 4
+0.5
+>>> 10 / 5
+2.0
+>>> 1/2
+0.5
+>>> type(2)
+<class 'int'>
+>>> type(0.5)
+<class 'float'>
+>>> 5%3
+2
+>>> 10%2
+0
+>>> 7.5%5
+2.5
+>>>
+
+print("hello world")
